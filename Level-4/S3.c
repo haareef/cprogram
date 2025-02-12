b38fdfd6cb9736c915510a74c915b2ee93a3dac1
haareef <haareef1234@gmail.com>
2025-02-12 15:18:21 +0530

    Update S3.c

--- Level-4/S3.c
+++ Level-4/S3.c
@@ -1,14 +1,1 @@
-//do while program
-#include <stdio.h>
 
-int main() {
-    int x;
-    printf("enter a number :");
-    scanf("%d",&x);
-    do {
-        printf("%d\n",x);
-        x--;
-    } while (x>0);
-
-    return 0;
-}
