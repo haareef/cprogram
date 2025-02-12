0ad432543df5885b8cbaabcef4ca3c6503b4ce18
haareef <haareef1234@gmail.com>
2025-02-12 14:48:44 +0530

    Initial commit

---
+++ Problems/tables.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter a number 1:");
+    scanf("%d",&n);
+    
+    for ( int i=1 ; i<11 ; i++){
+        
+            printf("%d * %d = %d\n",n,i,i*n);
+        }
+        
+    
+
+ return 0;
+}
