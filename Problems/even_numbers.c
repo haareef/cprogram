e7a008092b4ab5a0c4335352b63d356a8506ddb4
haareef <haareef1234@gmail.com>
2025-02-12 14:45:09 +0530

    Initial commit

---
+++ Problems/even_numbers.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    
+    for ( int i=1 ; i<51 ; i++){
+        if (i%2==0){
+            printf("%d \n",i);
+        }
+        
+    }
+
+    return 0;
+}
