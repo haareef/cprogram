9eb0e3edebddeb932ed9fe889300b5082c6e3419
haareef <haareef1234@gmail.com>
2025-02-12 15:11:52 +0530

    Initial commit

---
+++ Problems/S1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("enter a number :");
+    scanf("%d",&x);
+    while (x>10){
+        printf("%d",x);
+        break;
+    }
+
+
+    return 0;
+}
